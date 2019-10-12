f94b6dd72e06e4a942918e0efcd7f4125d0aac00
Suyeol Jeon <devxoul@gmail.com>
2019-10-13 07:36:10 +0900

    Create TestAppDelegate.swift

---
+++ Projects/AloneSocialClub/Tests/TestAppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TestAppDelegate.swift
+//  AloneSocialClubTests
+//
+//  Created by Suyeol Jeon on 2019/10/13.
+//
+
+import UIKit
+
+final class TestAppDelegate: NSObject, UIApplicationDelegate {
+  var window: UIWindow?
+}
